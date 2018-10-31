d85bf3430938922bac242e5936d718801aee3d0a
Abel George <abel.paul.george@gmail.com>
2018-10-31 19:15:12 -0400

    wildcard added

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/",function(req,res){
     res.send("Hi there, welcome to my assignment!");
 })
 
+
+app.get("*",function(req,res){
+    res.send("Sorry, page not found...What are you doing with your life?")
+})
 app.listen(3000, function(){
     console.log("Hi from Port 3000");
 })
