23b04edeb341ea82bd2e8b0cbb327e667a3fa63e
Abel George <abel.paul.george@gmail.com>
2018-11-01 09:34:36 -0400

    added speech

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ var app = express();
 
 app.get("/",function(req,res){
     res.send("Hi there, welcome to my assignment!");
-})
+});
+
+app.get("/speak/:animal", function(req,res){
+    
+    var sounds = {
+        pig: "oink!",
+        cow: "moo!",
+        dog: "bow bow!",
+        cat: "meow!",
+        human: "yeet!",
+        goldfish: "glub glub glub!"
+};
+
+var animal = req.params.animal;
+    var sound = sounds[animal];
+    res.send("The " + animal + " says "+ sound);
+});
 
 
 app.get("*",function(req,res){
     res.send("Sorry, page not found...What are you doing with your life?")
-})
+});
+
 app.listen(3000, function(){
     console.log("Hi from Port 3000");
-})
+});
